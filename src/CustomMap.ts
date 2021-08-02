72d259f704e29937e5900d32099edc9ce2d900fa
Wahéb <waheb.benzaid@gmail.com>
2021-08-02 19:18:01 +0100

    remove some comments

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -31,18 +31,4 @@ export class CustomMap {
             }
         })
     }
-
-    // addCompanyMarker(company:Company) : void {
-    //     new google.maps.Marker({
-    //         map: this.googleMap,
-    //         position: {
-    //             lat: company.location.lat,
-    //             lng : company.location.lng
-    //         }
-    //     })
-
-    // }
-
-
-
 }
